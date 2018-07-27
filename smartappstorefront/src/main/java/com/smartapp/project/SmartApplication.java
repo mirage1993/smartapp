f179aef37fad6f12f64110108726bf508b2319f6
Yongyong.Wang <Yongyong.Wang@connext.com.cn>
2018-07-27 15:27:43 +0800

    init

--- smartappstorefront/src/main/java/com/smartapp/project/SmartApplication.java
+++ smartappstorefront/src/main/java/com/smartapp/project/SmartApplication.java
@@ -2,6 +2,8 @@ package com.smartapp.project;
 
 import com.smartapp.project.core.data.MessageData;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,9 +12,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class SmartApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(SmartApplication.class);
+
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SmartApplication.class);
         application.run(args);
+        logger.info("the smart app is running...");
     }
 
 }
