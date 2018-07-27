58ed5e01e98ff43cbb98708b0ef0d71a4aae76ea
Yongyong.Wang <Yongyong.Wang@connext.com.cn>
2018-07-27 11:44:59 +0800

    init

--- smartappstorefront/src/main/java/com/smartapp/project/SmartApplication.java
+++ smartappstorefront/src/main/java/com/smartapp/project/SmartApplication.java
@@ -1,10 +1,13 @@
 package com.smartapp.project;
 
 import com.smartapp.project.core.data.MessageData;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SmartApplication {
 
     public static void main(String[] args) {
