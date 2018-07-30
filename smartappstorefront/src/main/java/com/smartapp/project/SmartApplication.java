ffbb98771b88ed5e055fd0da586a74e64cecec53
Yongyong.Wang <Yongyong.Wang@connext.com.cn>
2018-07-30 16:13:04 +0800

    init

--- smartappstorefront/src/main/java/com/smartapp/project/SmartApplication.java
+++ smartappstorefront/src/main/java/com/smartapp/project/SmartApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement
 public class SmartApplication {
 
