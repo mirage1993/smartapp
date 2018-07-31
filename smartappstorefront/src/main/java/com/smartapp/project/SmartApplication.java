95ccbaa4f249447474a4e783ce140acf33725571
Yongyong.Wang <Yongyong.Wang@connext.com.cn>
2018-07-31 17:49:38 +0800

    init

--- smartappstorefront/src/main/java/com/smartapp/project/SmartApplication.java
+++ smartappstorefront/src/main/java/com/smartapp/project/SmartApplication.java
@@ -1,11 +1,9 @@
 package com.smartapp.project;
 
-import com.smartapp.project.util.SpringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
